b0e1a34cd176c744f76884018af44803ad2e95d8
ValerioCristofori <cristoforivalerio@gmail.com>
2022-01-04 13:54:20 -0500

    adding serial CSR matxvec

---
+++ project/utils.h
@@ -0,0 +1,19 @@
+#include <time.h>
+
+typedef struct{
+	time_t tv_sec; /* seconds */
+	long tv_nsec; /* nanoseconds */
+}timespect;
+
+typedef enum{
+  true = 1, false = 0
+}bool;
+
+bool checkerror(const double *resp, const double *ress, int dim);
+
+void getmul(const double *val, const double *vec, const int *rIndex,
+	const int *cIndex, int nz, double *res);
+
+void quicksort(double* a, double* vindex, int* rindex, int* cindex, int n);
+
+void dprintArray(int* a, int len);
